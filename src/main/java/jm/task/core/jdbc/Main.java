052803a60fdfeb1869ffc690642555f417d6c524
KirillDavydov1 <82818161+KirillDavydov1@users.noreply.github.com>
2023-10-15 23:08:54 +0300

    Connection to Database MySQL with JDBC.

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,26 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
+        User user = new User("Ivan", "Ivanov", (byte) 14);
+        User user1 = new User("Ivan1", "Ivanov1", (byte) 20);
+        User user2 = new User("Ivan2", "Ivanov2", (byte) 30);
+        User user3 = new User("Ivan3", "Ivanov3", (byte) 99);
+        List<User> userList = Arrays.asList(user, user1, user2, user3);
+        for (User element : userList) {
+            userService.saveUser(element.getName(), element.getLastName(), element.getAge());
+        }
+        userService.getAllUsers().forEach(System.out::println);
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,26 @@
 package jm.task.core.jdbc.util;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+
+    public static Connection getConnection() {
+        Properties databaseProperties = new Properties();
+        try (InputStream in = Files.newInputStream(Paths.get("src/database.properties"))) {
+            databaseProperties.load(in);
+            Connection connection = DriverManager.getConnection(databaseProperties.getProperty("url"),
+                    databaseProperties.getProperty("username"), databaseProperties.getProperty("password"));
+            connection.setAutoCommit(false);
+            return connection;
+        } catch (IOException | SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
